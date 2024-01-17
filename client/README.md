f609cc2e0fc95550ced90783c4a91f73f68db6f2
Yuan Wu <wu.yuanf@gmail.com>
2024-01-16 20:19:24 -0500

    add:header

--- client/README.md
+++ client/README.md
@@ -1,1 +1,4 @@
 YT: https://www.youtube.com/watch?v=Kkht2mwSL_I
+UI components: 
+- flowbite:  https://www.flowbite-react.com/
+- react-icons: https://react-icons.github.io/react-icons/
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/sign-in' element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
