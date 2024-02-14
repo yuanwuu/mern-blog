5c42c4ae21fcc476ae0b1748593ca4e2952ff690
Yuan Wu <wu.yuanf@gmail.com>
2024-02-14 08:14:48 -0500

    add create a post API + upload post image functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser'
 
 
@@ -39,3 +40,4 @@ app.use(cookieParser()) // extract cookie from browser
 // ------------------------------ ROUTES + CONTROLLERS -----------------------------
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+app.use('/api/post',postRoutes)
